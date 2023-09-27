883dfce2f07c4d62bf808074a5445b4480890401
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-27 17:11:46 +0200

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,1 +1,18 @@
-.sh
+#!/bin/bash
+
+COMMAND="$@"
+
+if [ -z "$COMMAND" ]; then
+    echo "Please provide a command to run."
+    exit 1
+fi
+
+while true; do
+    # Check if the program is running
+    if ! pgrep -f "$COMMAND" > /dev/null; then
+        # Start the program if not running
+        $COMMAND &
+    fi
+    # Wait for a minute before checking again
+    sleep 60
+done
